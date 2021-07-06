c71635001600868703dc8a884d7854eaf459d7aa
OhyamaHideaki <86543038+OhyamaHideaki@users.noreply.github.com>
2021-07-07 01:13:28 +0900

    Add files via upload

--- wk1/index1.php
+++ wk1/index1.php
@@ -1,3 +1,35 @@
 <?php
- echo "こんにちは";
+
+$score = -20 ;
+
+
+if($score >=90 && $score <=100){
+    echo "A"."<br>";
+    if($score ==100){
+        echo "Congrats";
+    }
+}else if($score >=80 && $score <=89){
+    echo "B";
+}else if($score >=70 && $score <=79){
+    echo "C";
+}else if($score >=60 && $score <=69){
+    echo "D";
+}else if($score >=50 && $score <=59){
+    echo "E";
+}else{
+    echo "不合格"."<br>";
+    if($score <=0){
+        echo "Invalid Input"."<br>";
+        if($score ==0){
+            echo "Very bad";
+        }
+    }
+}
+
+
+
+
+
+
 ?>
+
